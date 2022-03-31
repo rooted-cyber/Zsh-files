1f5fba403402ccfe15427c8ca733ec8bdf8e12e2
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-03-31 19:55:35 +0530

    Add files via upload

--- theme.sh
+++ theme.sh
@@ -3,12 +3,12 @@ thi() {
 	cd ~
 	mkdir Rootedcyber
 	cd Rootedcyber
-	wget -q
+	wget -q https://raw.githubusercontent.com/rooted-cyber/Zsh-files/main/short.sh
 	git clone https://github.com/zsh-users/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting
 	git clone https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh
 	cd ~
-	wget -q
+	wget -q https://raw.githubusercontent.com/rooted-cyber/Zsh-files/main/.zshrc
 	chmod 700 -R *
 	chsh -s zsh
 	random
@@ -28,7 +28,7 @@ thi() {
 	printf "Please wait..."
 	cd ~/.termux
 	rm -f termux.properties > /dev/null 2>&1
-	wget -q 
+	wget -q https://raw.githubusercontent.com/rooted-cyber/Zsh-files/main/termux.properties
 	chmod 700 *
 	termux-reload-setting
 	random
