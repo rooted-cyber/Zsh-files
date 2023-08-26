3a664821b56e29face07ccfe1341d7df474f9396
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-08-26 07:02:18 +0530

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -12,14 +12,16 @@ ct() {
 	cd ~
 	mkdir Rootedcyber
 	cd Rootedcyber
-	wget -q https://raw.githubusercontent.com/rooted-cyber/Zsh-files/main/short.sh
 	git clone https://github.com/zsh-users/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting
 	git clone https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh
-	wget -q https://raw.githubusercontent.com/rooted-cyber/Zsh-files/main/theme2.sh
 	chmod 700 -R *
 	cd ~
-	wget -q https://raw.githubusercontent.com/rooted-cyber/Zsh-files/main/.zshrc
+ git clone https://github.com/rooted-cyber/Zsh-files
+ chmod -R 755 *
+ cd Zsh*
+ cp short* theme2* ~/Rooted*
+ cp .zshrc ~
 	chsh -s zsh
 	random
 	echo "Successfully installed"
@@ -40,7 +42,10 @@ ct() {
 	ct
 	cd ~/.termux
 	rm -f termux.properties > /dev/null 2>&1
-	wget -q https://raw.githubusercontent.com/rooted-cyber/Zsh-files/main/termux.properties
+	git clone https://github.com/rooted-cyber/Zsh-files
+cd Zsh*
+chmod 755 *
+cp termux* ~/.termux
 	chmod 700 *
 	termux-reload-settings
 	random
